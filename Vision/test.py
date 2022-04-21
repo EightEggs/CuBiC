85cc8c8260a2598af5c053adef26fcf3e21e9bd3
JerryZhou <3524046949@qq.com>
2022-04-21 16:45:09 +0800

    Changes

--- Vision/test.py
+++ Vision/test.py
@@ -1,3 +1,6 @@
-import twophase.performance as pf
+## import twophase.performance as pf
+import twophase.server as srv
+srv.start(8080, 20, 1)
+import twophase.computer_vision
 
-pf.test(100,0.1)
+# pf.test(100,0.1)

b55f65317fca35075eb7d189b96d3a869ba08c17
zhou-yong-jian <3525046949@qq.com>
2022-06-05 23:25:26 +0800

    差不多了，坐标目前只是示例，不是太准，而且我用来做数据集的魔方和照片魔方有色差，还有一定灯光影响（照片红色和橙色有点像）

--- Vision/svm.py
+++ Vision/svm.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import cv2
 from roi_extract import roi_extract
 
-model_path = './svm_cube.model'
+model_path = './training_set/svm_cube.model'
 clf = joblib.load(model_path)  # 加载模型
 
 
@@ -24,6 +24,12 @@ def color_detect(img_path) -> str:  # 利用训练好的module进行颜色检测
         preResult = clf.predict(img2arr)
         preResults.append(' '.join(str(i) for i in list(preResult)))
     preResults = ''.join(str(i) for i in list(preResults))
+    preResults = preResults.replace('R', 'F')
+    preResults = preResults.replace('G', 'R')
+    preResults = preResults.replace('B', 'L')
+    preResults = preResults.replace('O', 'B')
+    preResults = preResults.replace('Y', 'U')
+    preResults = preResults.replace('W', 'D')
     return preResults
 
 
